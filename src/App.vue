7f9ff33e4a7bbcefa4da8579ae87f91776ad09d9
tejas.belani <tejas.belani@LAPN212276>
2022-12-30 15:45:43 +0530

    git add all project files.

---
+++ src/App.vue
@@ -0,0 +1,79 @@
+<script>
+import HelloWorldDemo from './components/HelloWorldDemo.vue';
+import {reactive} from 'vue'
+export default{
+  setup()
+  {
+    const info=reactive({count:0})
+    return{info}
+  },
+  components:{
+    HelloWorldDemo
+  },
+  data() {
+    return {
+      emp:{},
+      users:[],
+      info:45,
+      theme:"lighttheme colorinfo",
+      splist:[]
+    }
+  },
+  computed:{
+    CountInactive()
+    {
+     return this.users.filter(p=>p.status=="inactive").length;
+    }  
+  },
+  watch:{ 
+    users:{
+    handler(newValue,oldValue)
+        {
+          console.log("Value changed");
+        },deep:'true'
+  }},
+  methods: {
+    demo()
+    {
+      this.users.push(this.emp);
+      this.users[0].name="rahul";
+      this.emp={};
+    },
+    info1(data,abc)
+    {
+      this.splist=data;
+      console.log(abc)
+    }
+  },
+}
+</script>
+
+<template>
+
+  Count{{ splist }}
+  <!-- {{ info.count }} -->
+<HelloWorldDemo @notify="info1($event)" message="Good Morning" :data="users"></HelloWorldDemo>
+
+  <h1>Inactive USers{{CountInactive}}</h1>
+  Enter Name <input type="text" v-model="emp.name"/>
+  Enter Address <input type="text" v-model="emp.address"/>
+Active  <input type="radio" value="active" v-model="emp.status"/>
+InActive <input type="radio" value="inactive" v-model="emp.status"/>
+<button @click="demo()">Add employee</button>
+
+</template>
+
+<style>
+.lighttheme
+{
+  border:1px solid lightblue;
+ 
+}
+.colorinfo
+{
+  color:red;
+  border:1px solid green;
+ 
+
+}
+</style>
---
+++ src/components/HelloWorldDemo.vue
@@ -0,0 +1,31 @@
+<script>
+export default{
+    props:{message:String,data:[]},
+    data() {
+        return {
+            splist:[]
+        }
+    },
+    methods:{
+        sponsorslist(p)
+        {
+            this.splist.push(p);
+            this.$emit("notify",this.splist,"Abc");
+        }
+    }
+}
+</script>
+<template>
+<h1>{{message}}</h1>
+
+<table>
+    <tr v-for="p in data">
+      <td>{{p.name}}</td>
+      <td>{{p.address}}</td>
+      <td>{{p.status}}</td>
+        <td><button @click="sponsorslist(p)">Give sponser</button></td>
+    </tr>
+  </table>
+</template>
+<style>
+</style>
